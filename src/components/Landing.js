724e05b03d58a5bc4f9195dc6736cb811685dbe7
github username <torpid@gmail.com>
2019-01-27 22:05:25 -0500

    skeleto for landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 class Landing extends Component {
-    
+    render() {
+        return(
+            <div>About Me</div>
+        )
+    }
 }
 
 export default Landing;
