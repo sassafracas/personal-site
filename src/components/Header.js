aac4f4cb9009da615161c9f2abb82e18d080c9ea
github username <torpid@gmail.com>
2019-01-27 22:02:43 -0500

    added some links

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
     render() {
         return(
             <div>
-            
+                <div><Link to="/">Home</Link> <Link to="/about">About Me</Link> <Link to="/portfolio">Portfolio</Link></div>
             </div>
         )
     }
