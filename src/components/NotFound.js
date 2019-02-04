a395c1875b031294dd6c29dfe5c1556a3d07aa22
github username <torpid@gmail.com>
2019-02-03 20:44:25 -0500

    simple not found page

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    render() {
+        return(
+            <div>The page you're looking for doesn't exist.</div>
+        )
+    }
+}
+
+export default NotFound;
