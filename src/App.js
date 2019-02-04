60774126b6dabd314ed92d95248abf931baab287
github username <torpid@gmail.com>
2019-02-03 20:50:48 -0500

    route for not found made

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ConnectedRouter } from 'connected-react-router';
 import { Route, Switch } from 'react-router';
 import './App.css';
 import Header from './components/Header';
+import NotFound from './components/NotFound';
 
 
 const App = ({ history }) => {
@@ -12,9 +13,9 @@ const App = ({ history }) => {
         <Header />
         <Switch>
           <Route exact path="/" render={() => (<div>Home</div>)} />
-          <Route exact path="/about" render={() => (<div>About</div>)} />
-          <Route exact path="/portfolio" render={() => (<div>Portfolio</div>)} />
-          <Route render={() => (<div>Miss</div>)} />
+          <Route path="/about" render={() => (<div>About</div>)} />
+          <Route path="/portfolio" render={() => (<div>Portfolio</div>)} />
+          <Route component={NotFound} />
         </Switch>
       </div>
     </ConnectedRouter>
