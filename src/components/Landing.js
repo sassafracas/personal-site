e46ea39171437264d7f3b7961222568602983802
github username <torpid@gmail.com>
2019-02-10 23:30:46 -0500

    image added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Landing extends Component {
     render() {
         return(
-            <div>About Me</div>
+            <img>Image of me</img>
         )
     }
 }
