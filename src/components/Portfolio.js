ee5716dc5f5d9fbdf40f0dd8e1684ed8886e2c4a
github username <torpid@gmail.com>
2019-03-31 22:12:18 -0400

    fleshing out porfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,6 +4,9 @@ class Portfolio extends Component {
     render() {
         return (
             <div className="portfolio-container">
+                <div>Project 1</div>
+                <div>Project 2</div>
+                <div>Project 3</div>
             </div>
         )
     }
